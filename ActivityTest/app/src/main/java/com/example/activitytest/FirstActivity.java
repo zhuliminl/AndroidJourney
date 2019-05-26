c200f38c2fc175ee6136fb2707ac3978e104916e
saul <zhuliminl@gmail.com>
2019-05-26 16:59:26 +0800

    intent return data from next activity

--- ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -1,8 +1,11 @@
 package com.example.activitytest;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -31,6 +34,20 @@ public class FirstActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case 2:
+                if(resultCode == RESULT_OK) {
+                    String returnData = data.getStringExtra("data_extra");
+                    Log.d("FirstActivity", returnData);
+
+                }
+                break;
+            default:
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +58,44 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // 返回数据
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 2);
+
+                // 传递数据
+                /*
+                String data = "你好，活动2";
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("extra_data", data);
+                startActivity(intent);
+                */
+
+                // 打电话
+                /*
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+                */
+
+
+                /*
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+                */
+
+
+                /*
                 // Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
                 Intent intent = new Intent("com.example.activitytest.ACTION_START");
+                intent.addCategory("com.example.activitytest.MY_CATEGORY");
                 startActivity(intent);
 
 
                 Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_LONG).show();
-               //  finish();
+                // finish();
+               */
             }
 
         });
--- ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -1,7 +1,13 @@
 package com.example.activitytest;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+
+        /*
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Log.d("SecondActivity", data);
+        */
+
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.putExtra("data_extra", "Hello FirstActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+
+                /*
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+
+                Toast.makeText(SecondActivity.this, "转到网页", Toast.LENGTH_LONG).show();
+                */
+            }
+
+        });
     }
 }
